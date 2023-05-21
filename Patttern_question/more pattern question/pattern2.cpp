b265f37086ca7a27e33d6ca8ddabe5f5aeb0b1fc
hansu03 <114220372+hansu03@users.noreply.github.com>
2023-05-21 19:32:02 +0530

    Update pattern2.cpp

--- Patttern_question/more pattern question/pattern2.cpp
+++ Patttern_question/more pattern question/pattern2.cpp
@@ -1,11 +1,38 @@
-#include<iostream>
+//left down triangle
+#include <iostream>
 using namespace std;
- 
- int main()
- {
 
-    cout << "Hello ";
-    
-    return 0;
+int main() {
+  // size of the triangle
+  int size = 5;
+  for (int i = 0; i < size; i++) {
+    // print stars
+    for (int j = 0; j < size - i; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
 
- }
+
+// right down triangle
+#include <iostream>
+using namespace std;
+
+int main() {
+  // size of the triangle
+  int size = 5;
+  for (int i = 0; i < size; i++) {
+    // print spaces
+    for (int j = 0; j < i; j++) {
+      cout << " ";
+    }
+    // print stars
+    for (int j = size; j > i; j--) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
